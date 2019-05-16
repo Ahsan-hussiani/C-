017fe717b3e612ee076aa2d8312d3a63fa344831
Ahsan-hussiani <50571527+Ahsan-hussiani@users.noreply.github.com>
2019-05-16 13:10:11 +0430

    Add files via upload

---
+++ main 3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+int i,u,t,ARR[5]={11,29,2,5,10};
+for (u=4;u>=1;u--)
+for (i=0;i<u;i++)
+if (ARR[i]>ARR[i+1])
+{
+    t=ARR[i];
+    ARR[i]=ARR[i+1];
+    ARR[i+1]=t;
+}
+cout <<"Sorted values"<<endl;
+for(i=0;i<=4;i++)
+    cout <<ARR[i]<<endl;
+    return 0;
+}
